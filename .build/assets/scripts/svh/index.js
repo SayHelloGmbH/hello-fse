00421912216343deaa78a633f4d4bdd3cc4255b6
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-07-05 14:22:11 +0200

    Optimize VH implementation

---
+++ .build/assets/scripts/svh/index.js
@@ -0,0 +1,9 @@
+const set = () => {
+	document.documentElement
+		.querySelector('body')
+		.style.setProperty('--vh', window.innerHeight / 100 + 'px');
+};
+
+set();
+window.addEventListener('resize', set);
+window.addEventListener('orientationchange', set);
--- .build/assets/scripts/ui/index.js
+++ .build/assets/scripts/ui/index.js
@@ -3,7 +3,6 @@
 import './_polyfill';
 import './a11y';
 import './aria-toggler';
-import './vh';
 
 console.log('%cDeveloped by', 'font-style: italic; font-size: 12px;');
 console.log(
@@ -11,4 +10,13 @@ console.log(
 	'font-weight: bold; color: #000; font-size: 16px;'
 );
 console.log('%chttps://sayhello.ch', 'color: #000; font-size: 12px;');
-console.log('');
+
+// Load script if svh is not supported natively
+if (!CSS.supports || !CSS.supports('height', '1svh')) {
+	let script = document.createElement('script');
+	script.setAttribute(
+		'src',
+		`${sht_theme.directory_uri}/assets/scripts/svh.min.js?version=${sht_theme.version}`
+	);
+	document.head.appendChild(script);
+}
