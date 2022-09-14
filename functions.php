cc46ce3efae991287d5a4a3e7b4e71e0a09a40ad
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-09-14 17:17:07 +0200

    Remove manual version control
    This is already integrated in core throught the comments in style.css

--- functions.php
+++ functions.php
@@ -14,32 +14,7 @@ if (!function_exists('dump')) {
 	}
 }
 
-$php_required = '8.0';
-$wp_required = '6.0';
-
-if (version_compare(get_bloginfo('version'), $wp_required, '<') || version_compare(PHP_VERSION, $php_required, '<')) {
-	add_action(
-		'admin_notices',
-		function () use ($php_required, $wp_required) {
-			// translators: Admin notice for system requirements
-			echo '<div class="error"><p>' . sprintf(
-				__('Das ausgewählte Theme benötigt PHP %1$s (oder neuer) und WordPress %2$s (oder neuer). Ihre Website verfügt über PHP %3$s und WordPress %4$s. Bitte aktualisieren Sie die Abhängigkeiten, damit keine fatale Fehler erzeugt werden.', 'sha'),
-				$php_required,
-				$wp_required,
-				PHP_VERSION,
-				$GLOBALS['wp_version']
-			) . '</p></div>';
-		}
-	);
-
-	add_action(
-		'after_switch_theme',
-		function () {
-			switch_theme(get_option('theme_switched'));
-		}
-	);
-} else {
-	/*
+/*
 	 * This lot auto-loads a class or trait just when you need it. You don't need to
 	 * use require, include or anything to get the class/trait files, as long
 	 * as they are stored in the correct folder and use the correct namespaces.
@@ -47,47 +22,46 @@ if (version_compare(get_bloginfo('version'), $wp_required, '<') || version_compa
 	 * See http://www.php-fig.org/psr/psr-4/ for an explanation of the file structure
 	 * and https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md for usage examples.
 	 */
-	spl_autoload_register(function ($class) {
+spl_autoload_register(function ($class) {
 
-		// project-specific namespace prefix
-		$prefix = 'SayHello\\Theme\\';
+	// project-specific namespace prefix
+	$prefix = 'SayHello\\Theme\\';
 
-		// base directory for the namespace prefix
-		$base_dir = __DIR__ . '/src/';
+	// base directory for the namespace prefix
+	$base_dir = __DIR__ . '/src/';
 
-		// does the class use the namespace prefix?
-		$len = strlen($prefix);
-		if (strncmp($prefix, $class, $len) !== 0) {
-			// no, move to the next registered autoloader
-			return;
-		}
-
-		// get the relative class name
-		$relative_class = substr($class, $len);
-
-		// replace the namespace prefix with the base directory, replace namespace
-		// separators with directory separators in the relative class name, append
-		// with .php
-		$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-		// if the file exists, require it
-		if (file_exists($file)) {
-			require $file;
-		}
-	});
-
-	/**
-	 * Returns the Theme Instance
-	 *
-	 * @return Object Theme Object
-	 */
-	if (!function_exists('sht_theme')) {
-		function sht_theme()
-		{
-			return SayHello\Theme\Theme::getInstance();
-		}
+	// does the class use the namespace prefix?
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+		// no, move to the next registered autoloader
+		return;
 	}
 
-	sht_theme();
-	sht_theme()->run();
+	// get the relative class name
+	$relative_class = substr($class, $len);
+
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+	// if the file exists, require it
+	if (file_exists($file)) {
+		require $file;
+	}
+});
+
+/**
+ * Returns the Theme Instance
+ *
+ * @return Object Theme Object
+ */
+if (!function_exists('sht_theme')) {
+	function sht_theme()
+	{
+		return SayHello\Theme\Theme::getInstance();
+	}
 }
+
+sht_theme();
+sht_theme()->run();
