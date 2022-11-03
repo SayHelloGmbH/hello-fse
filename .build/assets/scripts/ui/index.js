73c756a96df44493659348986c8597cb5653b9c1
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-11-03 11:33:38 +0100

    Conditionally load aria toggler

--- .build/assets/scripts/ui/index.js
+++ .build/assets/scripts/ui/index.js
@@ -1,7 +1,6 @@
 // import './modules/settings';
 import './_polyfill';
 import './a11y';
-import './aria-toggler';
 import './masthead';
 
 console.log('%cDeveloped by', 'font-style: italic; font-size: 12px;');
@@ -11,12 +10,22 @@ console.log(
 );
 console.log('%chttps://sayhello.ch', 'color: #000; font-size: 12px;');
 
-// Load script if svh is not supported natively
-if (!CSS.supports || !CSS.supports('height', '1svh')) {
-	let script = document.createElement('script');
-	script.setAttribute(
-		'src',
-		`${sht_theme.directory_uri}/assets/scripts/svh.min.js?version=${sht_theme.version}`
-	);
-	document.head.appendChild(script);
-}
+const conditionalLoadScript = (filename, condition) => {
+	if (!!condition) {
+		const min = sht_theme.debug ? '' : '.min';
+		let script = document.createElement('script');
+		script.setAttribute(
+			'src',
+			`${sht_theme.directory_uri}/assets/scripts/${filename}${min}.js?version=${sht_theme.version}`
+		);
+		document.head.appendChild(script);
+	}
+};
+
+conditionalLoadScript('svh', !CSS.supports || !CSS.supports('height', '1svh'));
+conditionalLoadScript(
+	'aria-toggler',
+	!!document.querySelectorAll(
+		'[aria-controls]:not([data-standalone-controller])'
+	).length
+);
--- assets/scripts/svh.js
+++ assets/scripts/svh.js
@@ -1,1 +1,1 @@
-(()=>{const e=()=>{document.documentElement.querySelector("body").style.setProperty("--vh",window.innerHeight/100+"px")};e(),window.addEventListener("resize",e),window.addEventListener("orientationchange",e)})();
+!function(){const e=()=>{document.documentElement.querySelector("body").style.setProperty("--vh",window.innerHeight/100+"px")};e(),window.addEventListener("resize",e),window.addEventListener("orientationchange",e)}();
