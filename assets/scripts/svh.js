2f7fc6e005ba60db1427b5a8a986d71f7ace4bdf
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-05-10 14:36:31 +0200

    Recompile assets

--- assets/scripts/svh.js
+++ assets/scripts/svh.js
@@ -1,1 +1,1 @@
-!function(){const e=()=>{document.documentElement.querySelector("body").style.setProperty("--vh",window.innerHeight/100+"px")};e(),window.addEventListener("resize",e),window.addEventListener("orientationchange",e)}();
+(()=>{const e=()=>{document.documentElement.querySelector("body").style.setProperty("--vh",window.innerHeight/100+"px")};e(),window.addEventListener("resize",e),window.addEventListener("orientationchange",e)})();
