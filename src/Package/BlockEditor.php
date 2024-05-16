c99fef3f7b6b26afcd4ee2b9cb239d400e13a599
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-05-16 11:20:44 +0200

    Don’t constrain allowed blocks

--- src/Package/BlockEditor.php
+++ src/Package/BlockEditor.php
@@ -28,6 +28,7 @@ class BlockEditor
 		}
 		add_action('enqueue_block_assets', [$this, 'enqueueBlockAssets']);
 		add_filter('block_editor_settings_all', [$this, 'editorSettings']);
+		// add_filter('allowed_block_types_all', [$this, 'allowedBlockTypes'], 10, 2);
 		add_action('after_setup_theme', [$this, 'themeSupports']);
 		add_action('init', [$this, 'setScriptTranslations']);
 		add_action('after_setup_theme', [$this, 'enqueueBlockStyles']);
