4e166d2cf2c3f41c6ec171361947177a9a60ce1d
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-06-26 13:19:29 +0200

    Set default group block gap

---
+++ patterns/footer.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: footer
+ * Slug: sht/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"className":"c-footer__inner","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group c-footer__inner"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|regular"}},"layout":{"inherit":true,"type":"constrained"}} -->
+	<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center"><?php echo __('Footer', 'sht'); ?></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"mobilemenu","tagName":"div"} /-->
---
+++ patterns/mobilemenu.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: mobilemenu
+ * Slug: sht/mobilemenu
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|regular"}},"backgroundColor":"cyan-bluish-gray","className":"c-mobilemenu is-hidden-for\u002d\u002ddesktop","layout":{"inherit":true,"type":"constrained"}} -->
+<div id="mobilemenu" class="wp-block-group c-mobilemenu is-hidden-for--desktop has-cyan-bluish-gray-background-color has-background"><!-- wp:paragraph {"align":"center"} -->
+	<p class="has-text-align-center"><?php echo __('Mobile menu content here', 'sht'); ?></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
