27b73bd5db7adea031b500c9febaca6dd7599ea3
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-08-15 17:22:40 +0200

    Strip out comments and old dump function

--- functions.php
+++ functions.php
@@ -6,51 +6,20 @@ if (!defined('DISALLOW_FILE_EDIT')) {
 	define('DISALLOW_FILE_EDIT', true);
 }
 
-if (!function_exists('dump')) {
-	function dump($var, $exit = false)
-	{
-		echo '<pre>';
-		var_dump($var);
-		echo '</pre>';
-
-		if ($exit) {
-			exit;
-		}
-	}
-}
-
-/*
- * This lot auto-loads a class or trait just when you need it. You don't need to
- * use require, include or anything to get the class/trait files, as long
- * as they are stored in the correct folder and use the correct namespaces.
- *
- * See http://www.php-fig.org/psr/psr-4/ for an explanation of the file structure
- * and https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md for usage examples.
- */
 spl_autoload_register(function ($class) {
-
-	// project-specific namespace prefix
 	$prefix = 'SayHello\\Theme\\';
 
-	// base directory for the namespace prefix
 	$base_dir = __DIR__ . '/src/';
 
-	// does the class use the namespace prefix?
 	$len = strlen($prefix);
 	if (strncmp($prefix, $class, $len) !== 0) {
-		// no, move to the next registered autoloader
 		return;
 	}
 
-	// get the relative class name
 	$relative_class = substr($class, $len);
 
-	// replace the namespace prefix with the base directory, replace namespace
-	// separators with directory separators in the relative class name, append
-	// with .php
 	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-	// if the file exists, require it
 	if (file_exists($file)) {
 		require $file;
 	}
