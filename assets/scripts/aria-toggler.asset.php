f96b88f550a838d1b1d969384d61ee52ecb2a012
Mark Howells-Mead <permanent.tourist@gmail.com>
2026-02-19 15:56:00 +0100

    Recompile JS according to newer logic

--- assets/scripts/aria-toggler.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '90ca8c5ad70de09d5f6f');
--- assets/scripts/ui.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1a850893b6ac01d19e0b');
