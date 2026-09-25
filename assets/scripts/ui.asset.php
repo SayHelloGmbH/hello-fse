1d084f28841684230433dc688e022572e4ffff00
Mark Howells-Mead <permanent.tourist@gmail.com>
2026-09-25 08:53:15 +0200

    Rebuild assets

--- assets/scripts/ui.asset.php
+++ assets/scripts/ui.asset.php
@@ -1,1 +1,6 @@
-<?php return array('dependencies' => array(), 'version' => '00ce87e3313440481d2f');
+<?php return array(
+	'dependencies' => array(
+		
+	),
+	'version' => '00ce87e3313440481d2f'
+);
